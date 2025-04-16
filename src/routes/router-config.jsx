938d667d4d0efc2feaefc07b63042517babb16ab
hoyoungleee <ghdud939@gmail.com>
2025-04-16 15:48:13 +0900

    베이스 url 커밋

--- src/routes/router-config.jsx
+++ src/routes/router-config.jsx
@@ -10,47 +10,52 @@ import CommunityPage from '../pages/CommunityPage';
 import CommunityPageDetail from '../pages/CommunityPageDetail';
 import ProductListPage from '../pages/ProductListPage';
 // 라우터 설정
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <RootLayout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        index: true, // 부모 경로가 활성화 되었을 때 기본적으로 사용할 컴포넌트
-        element: <IndexPage />,
-      },
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <RootLayout />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          index: true, // 부모 경로가 활성화 되었을 때 기본적으로 사용할 컴포넌트
+          element: <IndexPage />,
+        },
 
-      {
-        // :postId -> 경로에 붙는 파라미터 (/blog/1, /blog/7)
-        path: 'blog',
-        element: <BlogPage />,
-      },
-      {
-        path: 'blog/:postId',
-        element: <BlogPostDetail />,
-      },
-      {
-        path: 'about',
-        element: <AboutPage />,
-      },
-      {
-        path: 'products/:id',
-        element: <ProductDetailsPage />,
-      },
-      {
-        path: 'community',
-        element: <CommunityPage />,
-      },
-      {
-        // :postId -> 경로에 붙는 파라미터 (/blog/1, /blog/7)
-        path: 'community/:postId',
-        element: <CommunityPageDetail />,
-      },
-      {
-        path: 'used-items', // '/used-items' 경로 추가
-        element: <ProductListPage />, // ProductList 컴포넌트 연결
-      },
-    ],
+        {
+          // :postId -> 경로에 붙는 파라미터 (/blog/1, /blog/7)
+          path: 'blog',
+          element: <BlogPage />,
+        },
+        {
+          path: 'blog/:postId',
+          element: <BlogPostDetail />,
+        },
+        {
+          path: 'about',
+          element: <AboutPage />,
+        },
+        {
+          path: 'products/:id',
+          element: <ProductDetailsPage />,
+        },
+        {
+          path: 'community',
+          element: <CommunityPage />,
+        },
+        {
+          // :postId -> 경로에 붙는 파라미터 (/blog/1, /blog/7)
+          path: 'community/:postId',
+          element: <CommunityPageDetail />,
+        },
+        {
+          path: 'used-items', // '/used-items' 경로 추가
+          element: <ProductListPage />, // ProductList 컴포넌트 연결
+        },
+      ],
+    },
+  ],
+  {
+    basename: '/test-github-page',
   },
-]);
+);
